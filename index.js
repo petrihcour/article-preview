ec98df1b11a1bd7fa4b0bc9987de42e478b1783f
Vanessa Fischbach <vanessafischbach@Vanessas-MacBook-Pro.local>
2025-03-24 15:06:03 -0500

    add event to index js to make sure popup is not displayed immediately

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const shareButton = document.querySelector('.icon-button');
 const popup = document.getElementById('popup');
 const closePopup = document.getElementById('close-popup');
 
+popup.style.display = 'none';
+
 shareButton.addEventListener('click', function() {
     popup.style.display = 'block';
 })
